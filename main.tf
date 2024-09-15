dfd7a4b22ce9f08f00656750b351be39cc1efcfb
linjun0703 <11300242@qq.com>
2024-09-15 19:39:37 +0800

    1
    1

--- main.tf
+++ main.tf
@@ -57,7 +57,6 @@ module "eks" {
   cluster_version = var.cluster_version
   vpc_id          = data.aws_vpc.existing.id
   subnet_ids      = data.aws_subnets.all.ids
-  control_plane_subnet_ids = data.aws_subnets.all.ids  # 如果需要的话
   node_groups     = var.node_groups
 }
 
